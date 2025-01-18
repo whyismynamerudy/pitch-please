2375cdd3b61ea6f585999f28d3be657bfaa9ffac
Lucy Qi <l8qi@uwaterloo.ca>
2025-01-18 14:02:37 -0500

    fixed styling

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+
+export default function Page() {
+  return (
+    <BackgroundBeamsWithCollision>
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-white mb-4">What's cooler than Beams?</h1>
+        <p className="text-4xl text-violet-400">Exploding beams.</p>
+      </div>
+    </BackgroundBeamsWithCollision>
+  );
+} 
